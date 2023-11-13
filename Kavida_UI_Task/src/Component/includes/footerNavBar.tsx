e42481b33eec6e899c81b8e695035fdc90c2788c
Lavanya Ravi <lavanyarj2@gmail.com>
2023-11-13 17:20:08 +0530

    Updated the UI Task

---
+++ Kavida_UI_Task/src/Component/includes/footerNavBar.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Index(){
+    return(<>
+        <div className="col-md-12 footer d-flex justify-content-center align-items-center">
+            <div>
+                Kavida Technologies &#169; 2021
+            </div>
+        </div>
+    </>)
+}
